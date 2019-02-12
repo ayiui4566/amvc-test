fd41c4873b234ebbeb6c5718d709068d8d87c8da
hejiaxuan7 <hejiaxuan7@sina.com>
2019-02-12 16:26:27 +0800

    添加注解支持，修改测试代码

--- src/main/java/com/hebaibai/demo/web/IndexController.java
+++ src/main/java/com/hebaibai/demo/web/IndexController.java
@@ -1,21 +1,26 @@
 package com.hebaibai.demo.web;
 
+import com.hebaibai.amvc.annotation.Request;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author hjx
  */
+@Request
 public class IndexController {
 
     /**
      * @param name
      * @return
      */
-    public Map<String, String> index(String name) {
+    @Request
+    public Map<String, String> index(String name, int age) {
         Map<String, String> map = new HashMap<>();
         map.put("value", name);
         map.put("msg", "success");
+        map.put("msg2", age + "");
         return map;
     }
 
